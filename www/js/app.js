fb013096c12b8505d1c9a44af4dba4aa0eb61985
AntoineMahieu <mahieu.antoine@live.fr>
2016-05-18 16:15:45 +0200

    Mon travail colossal face a la glanditude de Monsieur Leblanc qui m'exaspere au plus haut point

--- www/js/app.js
+++ www/js/app.js
@@ -86,6 +86,73 @@ angular.module('starter', ['ionic', 'starter.controllers'/*,'textAngular'*/])
     $scope.showContent = function($fileContent){
       $scope.content = $fileContent;
     };
+
+    $scope.header = "CREATEDATE	LOCAL_WINDSPEED_INSTANT	LOCAL_WINDDIR_INSTANT	LOCAL_WS_2MIN_MNM	LOCAL_WS_2MIN_AVG	LOCAL_WS_2MIN_MAX	LOCAL_WS_10MIN_MNM	LOCAL_WS_10MIN_AVG	LOCAL_WS_10MIN_MAX	LOCAL_WD_2MIN_MNM	LOCAL_WD_2MIN_AVG	LOCAL_WD_2MIN_MAX	LOCAL_WD_10MIN_MNM	LOCAL_WD_10MIN_AVG	LOCAL_WD_10MIN_MAX	LOCAL_WIND_GUST	LOCAL_WIND_SQUALL	REMOTE_WINDSPEED_INSTANT	REMOTE_WINDDIR_INSTANT	REMOTE_WIND_2MIN_MNM	REMOTE_WIND_2MIN_AVG	REMOTE_WIND_2MIN_MAX	REMOTE_WIND_10MIN_MNM	REMOTE_WIND_10MIN_AVG	REMOTE_WIND_10MIN_MAX	REMOTE_WD_2MIN_MNM	REMOTE_WD_2MIN_AVG	REMOTE_WD_2MIN_MAX	REMOTE_WD_10MIN_MNM	REMOTE_WD_10MIN_AVG	REMOTE_WD_10MIN_MAX	REMOTE_WIND_GUST	REMOTE_WIND_SQUALL	AIR_TEMPERATURE	DEW_POINT	REL_HUMIDITY	RAIN_1H	RAIN_24H	AIR_PRESSURE	QNH	QFE	QFF	TREND_3H	TENDENCY	PRESSURE_ALT	DENSITY_ALT	PRESENT_WEATHER	RECENT_WEATHER	MOR_1MIN	MOR_10MIN	CLOUD_INSTANT_CL1	CLOUD_INSTANT_CL2	CLOUD_INSTANT_CL3	VERTICAL_VISIBILITY	LIGHTNING_DISTANCE	LIGHTNING_DIRECTION	LIGHTNING_COUNT	LIGHTNING_RF_NOISE	FREEZING_RAIN_ICING_STATUS	FREEZING_RAIN_ICING_MODE	CL1_OCTA1	CL1_BASE1	CL1_OCTA2	CL1_BASE2	CL1_OCTA3	CL1_BASE3	CL1_OCTA4	CL1_BASE4	CL1_OCTA5	CL1_BASE5	WATER_1H	WATER_3H	WATER_6H	WATER_24H	RATE	SNOW_1H	SNOW_3H	SNOW_6H	SNOW_24H";
+    $scope.headers = $scope.header.split("\t");
+    var count = 0;
+    $scope.headers.forEach(function(col){
+      count ++;
+      console.log("case " + count + " : " + col );
+    });
+
+    $scope.tamerelachaudiere = function() {
+
+      //var minTemp, timeMinTemp, minHumi, timeMinHumi, maxTemp, timeMaxTemp, maxHumi, timeMaxHumi, avgTemp, avgHumi, countTemp, countHumi;
+      var tab = $scope.content.split('\n');
+      var computedData;
+      // tab[x][y]    ===> x : temp(0), humi(1), pres(2), wspeed(3), wdir(4)
+                    //===> y : min(0), max(1), timeMin(2), timeMax(3), count(4)
+          computedData =  [
+             [parseFloat(tab[2].split("\t")[33]),parseFloat(tab[2].split("\t")[33]), tab[2].split("\t")[0] + " " + tab[2].split("\t")[1], tab[2].split("\t")[0] + " " + tab[2].split("\t")[1], 0]
+            ,[parseFloat(tab[2].split("\t")[35]),parseFloat(tab[2].split("\t")[35]), tab[2].split("\t")[0] + " " + tab[2].split("\t")[1], tab[2].split("\t")[0] + " " + tab[2].split("\t")[1], 0]
+            ,[parseFloat(tab[2].split("\t")[38]),parseFloat(tab[2].split("\t")[38]), tab[2].split("\t")[0] + " " + tab[2].split("\t")[1], tab[2].split("\t")[0] + " " + tab[2].split("\t")[1], 0]
+            ,[parseFloat(tab[2].split("\t")[3]), parseFloat(tab[2].split("\t")[3]), tab[2].split("\t")[0] + " " + tab[2].split("\t")[1], tab[2].split("\t")[0] + " " + tab[2].split("\t")[1], 0]
+            ,[parseFloat(tab[2].split("\t")[9]), parseFloat(tab[2].split("\t")[9]), tab[2].split("\t")[0] + " " + tab[2].split("\t")[1], tab[2].split("\t")[0] + " " + tab[2].split("\t")[1], 0]];
+
+      var counta = 0;
+      tab.shift();
+      tab.shift();
+      tab.shift();
+      console.log(computedData[0][0]);
+      tab.forEach(function(col){
+        counta ++;
+        var line = col.split("\t");
+        //temp
+        if(parseFloat(line[33]) < computedData[0][0]){
+          console.log("line : " + parseFloat(line[33]) + " < " + computedData[0][0]);
+            computedData[0][0] = parseFloat(line[33]);
+            computedData[0][2] = line[0] + " " + line[1]; //c'est la date ducon
+        }else if(parseFloat(line[33]) > computedData[0][1]){
+          computedData[0][1] = parseFloat(line[33]);
+          computedData[0][3] = line[0] + " " + line[1];
+        }
+        //humi
+        if(parseFloat(line[35]) < computedData[1][0]){
+          computedData[1][0] = parseFloat(line[35]);
+          computedData[1][2] = line[0] + " " + line[1];
+        }else if(parseFloat(line[35]) > computedData[1][1]){
+          computedData[1][1] = parseFloat(line[35]);
+          computedData[1][3] = line[0] + " " + line[1];
+        }
+
+        if(parseFloat(line[3]) < computedData[3][0]){
+          computedData[3][0] = parseFloat(line[35]);
+          computedData[3][2] = line[0] + " " + line[1];
+        }else if(parseFloat(line[3]) > computedData[3][1]){
+          computedData[1][1] = parseFloat(line[35]);
+          computedData[1][3] = line[0] + " " + line[1];
+        }
+
+
+
+        //console.log("case " + count + " : " + col );
+      });
+      console.log(computedData[0][0]);
+      console.log(computedData[0][1]);
+      console.log(computedData[0][2]);
+      console.log(computedData[0][3]);
+      console.log(counta);
+    };
   })
   .directive('onReadFile', function ($parse) {
   return {
