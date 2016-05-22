466a101a177b4a954715464e26baee9c3bc2cbaa
Pierre LEBLANC <leblan_p@etna-alternance.net>
2016-05-22 15:21:10 +0200

    Pierre : extraction des nom de colonne et index, En cours

--- www/js/app.js
+++ www/js/app.js
@@ -6,170 +6,178 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers'/*,'textAngular'*/])
 
-    .run(function($ionicPlatform) {
-        $ionicPlatform.ready(function() {
-            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-            // for form inputs)
-            if (window.cordova && window.cordova.plugins.Keyboard) {
-                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-                cordova.plugins.Keyboard.disableScroll(true);
+  .run(function($ionicPlatform) {
+    $ionicPlatform.ready(function() {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      if (window.cordova && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        cordova.plugins.Keyboard.disableScroll(true);
 
-            }
-            if (window.StatusBar) {
-                // org.apache.cordova.statusbar required
-                StatusBar.styleDefault();
-            }
-        });
-    })
+      }
+      if (window.StatusBar) {
+        // org.apache.cordova.statusbar required
+        StatusBar.styleDefault();
+      }
+    });
+  })
 
-    .config(function($stateProvider, $urlRouterProvider) {
-        $stateProvider
+  .config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
 
-            .state('app', {
-                url: '/app',
-                abstract: true,
-                templateUrl: 'templates/menu.html',
-                controller: 'AppCtrl'
-            })
+      .state('app', {
+        url: '/app',
+        abstract: true,
+        templateUrl: 'templates/menu.html',
+        controller: 'AppCtrl'
+      })
 
-            .state('app.search', {
-                url: '/search',
-                views: {
-                    'menuContent': {
-                        templateUrl: 'templates/search.html'
-                    }
-                }
-            })
-
-            .state('app.browse', {
-                url: '/browse',
-                views: {
-                    'menuContent': {
-                        templateUrl: 'templates/browse.html'
-                    }
-                }
-            })
-
-
-          .state('app.profile', {
-            url: '/profile',
-            views: {
-              'menuContent': {
-                templateUrl: 'templates/profile.html',
-                controller: 'ProfileCtrl'
-              }
-            }
-          })
-
-            .state('app.index', {
-                url: '/index',
-                views: {
-                    'menuContent': {
-                        templateUrl: 'templates/index.html',
-                        controller: 'TutorialsCtrl'
-                    }
-                }
-            });
-
-        // if none of the above states are matched, use this as the fallback
-        if (
-            ionic.Platform.isAndroid()
-            || ionic.Platform.isIOS()
-        ) {
-            $urlRouterProvider.otherwise('/app/mobile_login') ;
-        } else {
-            $urlRouterProvider.otherwise('/app/index');
+      .state('app.search', {
+        url: '/search',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/search.html'
+          }
         }
-    })
+      })
+
+      .state('app.browse', {
+        url: '/browse',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/browse.html'
+          }
+        }
+      })
+
+
+      .state('app.profile', {
+        url: '/profile',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/profile.html',
+            controller: 'ProfileCtrl'
+          }
+        }
+      })
+
+      .state('app.index', {
+        url: '/index',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/index.html',
+            controller: 'TutorialsCtrl'
+          }
+        }
+      });
+
+    // if none of the above states are matched, use this as the fallback
+    if (
+      ionic.Platform.isAndroid()
+      || ionic.Platform.isIOS()
+    ) {
+      $urlRouterProvider.otherwise('/app/mobile_login') ;
+    } else {
+      $urlRouterProvider.otherwise('/app/index');
+    }
+  })
 
   .controller('MainCtrl', function ($scope) {
     $scope.showContent = function($fileContent){
       $scope.content = $fileContent;
     };
 
+//    $scope.lines = "ma première line et \n" +
+//      "la seconde";
+//    $scope.lineOne = $scope.lines.split("\n");
+//    console.log(content);
+//    $scope.lineOne.forEach(function(ligne, i, j){
+//      console.log(ligne + i);
+//    });
+//    console.log(lineOne);
+
     $scope.header = "CREATEDATE	LOCAL_WINDSPEED_INSTANT	LOCAL_WINDDIR_INSTANT	LOCAL_WS_2MIN_MNM	LOCAL_WS_2MIN_AVG	LOCAL_WS_2MIN_MAX	LOCAL_WS_10MIN_MNM	LOCAL_WS_10MIN_AVG	LOCAL_WS_10MIN_MAX	LOCAL_WD_2MIN_MNM	LOCAL_WD_2MIN_AVG	LOCAL_WD_2MIN_MAX	LOCAL_WD_10MIN_MNM	LOCAL_WD_10MIN_AVG	LOCAL_WD_10MIN_MAX	LOCAL_WIND_GUST	LOCAL_WIND_SQUALL	REMOTE_WINDSPEED_INSTANT	REMOTE_WINDDIR_INSTANT	REMOTE_WIND_2MIN_MNM	REMOTE_WIND_2MIN_AVG	REMOTE_WIND_2MIN_MAX	REMOTE_WIND_10MIN_MNM	REMOTE_WIND_10MIN_AVG	REMOTE_WIND_10MIN_MAX	REMOTE_WD_2MIN_MNM	REMOTE_WD_2MIN_AVG	REMOTE_WD_2MIN_MAX	REMOTE_WD_10MIN_MNM	REMOTE_WD_10MIN_AVG	REMOTE_WD_10MIN_MAX	REMOTE_WIND_GUST	REMOTE_WIND_SQUALL	AIR_TEMPERATURE	DEW_POINT	REL_HUMIDITY	RAIN_1H	RAIN_24H	AIR_PRESSURE	QNH	QFE	QFF	TREND_3H	TENDENCY	PRESSURE_ALT	DENSITY_ALT	PRESENT_WEATHER	RECENT_WEATHER	MOR_1MIN	MOR_10MIN	CLOUD_INSTANT_CL1	CLOUD_INSTANT_CL2	CLOUD_INSTANT_CL3	VERTICAL_VISIBILITY	LIGHTNING_DISTANCE	LIGHTNING_DIRECTION	LIGHTNING_COUNT	LIGHTNING_RF_NOISE	FREEZING_RAIN_ICING_STATUS	FREEZING_RAIN_ICING_MODE	CL1_OCTA1	CL1_BASE1	CL1_OCTA2	CL1_BASE2	CL1_OCTA3	CL1_BASE3	CL1_OCTA4	CL1_BASE4	CL1_OCTA5	CL1_BASE5	WATER_1H	WATER_3H	WATER_6H	WATER_24H	RATE	SNOW_1H	SNOW_3H	SNOW_6H	SNOW_24H";
     $scope.headers = $scope.header.split("\t");
 
-    //Zone de taff recherche nom colonne par string
 
     var count = 0;
     $scope.headers.forEach(function(col){
       count ++;
-        //console.log("case " + count + " : " + col );
+      //console.log("case " + count + " : " + col );
     });
 
-      $scope.searchcol = function() {
-        $scope.headers.forEach(function(test){
-          var i = 0;
-          var j = 0;
-          var k = 0;
-          console.log(test);
-        if ("CREATEDATE" == test[j]) {
-          i = count;
-          console.log("--- GOT IT : " + i);
-        }
-        k++;
-        j++;
-      });
-    };
 
     $scope.tamerelachaudiere = function() {
 
       var tab = $scope.content.split('\n');
+
+      //Zone de taff recherche nom colonne par string
+      var lineOne = tab[1].split("\t");
+      lineOne.forEach(function(colname, i){
+        switch (colname) {
+          case "AIR_TEMPERATURE":
+          case "REL_HUMIDITY":
+          case "AIR_PRESSURE":
+          case "LOCAL_WS_2MIN_MNM":
+            console.log(colname + " : " + i);
+        }
+      });
+
       var computedData;
 
-          computedData =
-            [
-              [
-                parseFloat(tab[2].split("\t")[33]),
-                parseFloat(tab[2].split("\t")[33]),
-                tab[2].split("\t")[0] + " " + tab[2].split("\t")[1],
-                tab[2].split("\t")[0] + " " + tab[2].split("\t")[1],
-                0
-              ],
-              [
-                parseFloat(tab[2].split("\t")[35]),
-                parseFloat(tab[2].split("\t")[35]),
-                tab[2].split("\t")[0] + " " + tab[2].split("\t")[1],
-                tab[2].split("\t")[0] + " " + tab[2].split("\t")[1],
-                0
-              ],
-              [
-                parseFloat(tab[2].split("\t")[38]),
-                parseFloat(tab[2].split("\t")[38]),
-                tab[2].split("\t")[0] + " " + tab[2].split("\t")[1],
-                tab[2].split("\t")[0] + " " + tab[2].split("\t")[1],
-                0
-              ],
-              [
-                parseFloat(tab[2].split("\t")[3]),
-                parseFloat(tab[2].split("\t")[3]),
-                tab[2].split("\t")[0] + " " + tab[2].split("\t")[1],
-                tab[2].split("\t")[0] + " " + tab[2].split("\t")[1],
-                0
-              ],
-              [
-                parseFloat(tab[2].split("\t")[9]),
-                parseFloat(tab[2].split("\t")[9]),
-                tab[2].split("\t")[0] + " " + tab[2].split("\t")[1],
-                tab[2].split("\t")[0] + " " + tab[2].split("\t")[1],
-                0
-              ]
-            ];
+      computedData =
+        [
+          [
+            parseFloat(tab[2].split("\t")[33]),
+            parseFloat(tab[2].split("\t")[33]),
+            tab[2].split("\t")[0] + " " + tab[2].split("\t")[1],
+            tab[2].split("\t")[0] + " " + tab[2].split("\t")[1],
+            0
+          ],
+          [
+            parseFloat(tab[2].split("\t")[35]),
+            parseFloat(tab[2].split("\t")[35]),
+            tab[2].split("\t")[0] + " " + tab[2].split("\t")[1],
+            tab[2].split("\t")[0] + " " + tab[2].split("\t")[1],
+            0
+          ],
+          [
+            parseFloat(tab[2].split("\t")[38]),
+            parseFloat(tab[2].split("\t")[38]),
+            tab[2].split("\t")[0] + " " + tab[2].split("\t")[1],
+            tab[2].split("\t")[0] + " " + tab[2].split("\t")[1],
+            0
+          ],
+          [
+            parseFloat(tab[2].split("\t")[3]),
+            parseFloat(tab[2].split("\t")[3]),
+            tab[2].split("\t")[0] + " " + tab[2].split("\t")[1],
+            tab[2].split("\t")[0] + " " + tab[2].split("\t")[1],
+            0
+          ],
+          [
+            parseFloat(tab[2].split("\t")[9]),
+            parseFloat(tab[2].split("\t")[9]),
+            tab[2].split("\t")[0] + " " + tab[2].split("\t")[1],
+            tab[2].split("\t")[0] + " " + tab[2].split("\t")[1],
+            0
+          ]
+        ];
 
       var counta = 0;
       tab.shift();
       tab.shift();
       tab.shift();
       console.log(computedData[0][0]);
+
       tab.forEach(function(col){
         counta ++;
         var line = col.split("\t");
         //temp
         if (parseFloat(line[33]) < computedData[0][0]) {
           console.log("line : " + parseFloat(line[33]) + " < " + computedData[0][0]);
-            computedData[0][0] = parseFloat(line[33]);
-            computedData[0][2] = line[0] + " " + line[1]; //c'est la date ducon
+          computedData[0][0] = parseFloat(line[33]);
+          computedData[0][2] = line[0] + " " + line[1]; //c'est la date ducon
         } else if (parseFloat(line[33]) > computedData[0][1]) {
           computedData[0][1] = parseFloat(line[33]);
           computedData[0][3] = line[0] + " " + line[1];
@@ -203,24 +211,24 @@ angular.module('starter', ['ionic', 'starter.controllers'/*,'textAngular'*/])
     };
   })
   .directive('onReadFile', function ($parse) {
-  return {
-    restrict: 'A',
-    scope: false,
-    link: function(scope, element, attrs) {
-      var fn = $parse(attrs.onReadFile);
+    return {
+      restrict: 'A',
+      scope: false,
+      link: function(scope, element, attrs) {
+        var fn = $parse(attrs.onReadFile);
 
-      element.on('change', function(onChangeEvent) {
-        var reader = new FileReader();
+        element.on('change', function(onChangeEvent) {
+          var reader = new FileReader();
 
-        reader.onload = function(onLoadEvent) {
-          scope.$apply(function() {
-            fn(scope, {$fileContent:onLoadEvent.target.result});
-            console.log("AAAAHAHAHAHAHA");
-          });
-        };
+          reader.onload = function(onLoadEvent) {
+            scope.$apply(function() {
+              fn(scope, {$fileContent:onLoadEvent.target.result});
+              console.log(" - - - CHARGEMENT OK - - - ");
+            });
+          };
 
-        reader.readAsText((onChangeEvent.srcElement || onChangeEvent.target).files[0]);
-      });
-    }
-  };
-});
+          reader.readAsText((onChangeEvent.srcElement || onChangeEvent.target).files[0]);
+        });
+      }
+    };
+  });
